b7a2f9d66b5cfec54433caee9a28852cd84957d0
prudhvigattu <82654825+prudhvigattu@users.noreply.github.com>
2021-05-25 17:03:06 +0530

    Add files via upload

---
+++ kg_pounds.py
@@ -0,0 +1,4 @@
+'''write a program that asks the user for a weight in kgs and converts it to pounds.there are 2.2 pounds in kilogram'''
+kg=float(input("enter the weight in kilograms:"))
+pounds=kg*2.2 #1kg=2.2 pounds
+print(pounds)
---
+++ sum_average.py
@@ -0,0 +1,9 @@
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+n3=int(input("Enter third number:"))
+sum=n1+n2+n3
+average=float((n1+n2+n3)/3)
+print("sum of three numbers is",sum)
+print(average)
+
+
