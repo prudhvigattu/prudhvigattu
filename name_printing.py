1e867371dd43996ee9b057f73b9978ff754e456e
prudhvigattu <82654825+prudhvigattu@users.noreply.github.com>
2021-05-25 19:47:20 +0530

    Add files via upload

---
+++ name_printing.py
@@ -0,0 +1,5 @@
+def name():
+    name=input("Enter your name:")
+    n=int(input("enter no.of times to print:"))
+    for i in range(1,n+1,1):
+       print(name)
---
+++ number_series.py
@@ -0,0 +1,5 @@
+def series(i=8):
+    for i in range(i,90,3):
+        print(i,end=',')
+        
+    
